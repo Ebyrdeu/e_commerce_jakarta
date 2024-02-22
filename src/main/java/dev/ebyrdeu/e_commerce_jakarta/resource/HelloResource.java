b46d7088b36b4b5bcde1a4e8ad662c12cdc54e57
Maxim Khnykin <ebyrdeu.khnykin@gmail.com>
2024-02-22 12:35:02 +0100

    initial commit

---
+++ src/main/java/dev/ebyrdeu/e_commerce_jakarta/resource/HelloResource.java
@@ -0,0 +1,14 @@
+package dev.ebyrdeu.e_commerce_jakarta.resource;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+
+@Path("/hello-world")
+public class HelloResource {
+    @GET
+    @Produces("text/plain")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
